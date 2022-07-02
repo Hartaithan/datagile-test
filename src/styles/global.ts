c2c4a64dffaed32e0d1f2e0778859430d8c98dd3
hartaithan <hartaithan@gmail.com>
2022-07-02 19:35:17 +0900

    change global styles and install font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,10 +5,24 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+    font-family: "Inter", sans-serif;
+    color: #ffffff;
+    scrollbar-width: thin;
+    scrollbar-color: #333333 #ffffff;
+    -webkit-font-feature-settings: "lnum";
+    -moz-font-feature-settings: "lnum";
+    font-feature-settings: "lnum";
   }
   html, body, #root {
     height: 100%;
     width: 100%;
+  }
+  body {
+    background: #070707;
+  }
+  .App {
+    max-width: 1080px;
+    margin: 0 auto;
   }
 `;
 
